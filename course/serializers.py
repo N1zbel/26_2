1e05a02883122de318322b95a1307149aa1dc035
Andrey Ryazanov <ark2635@gmail.com>
2023-10-28 11:12:00 +0300

    upd serializers

--- course/serializers.py
+++ course/serializers.py
@@ -1,15 +1,26 @@
 from rest_framework import serializers
 
-from course.models import Course, Lesson
+from .models import Course, Lesson, Payment
 
 
 class CourseSerializer(serializers.ModelSerializer):
+    lesson_count = serializers.SerializerMethodField()
+
     class Meta:
         model = Course
         fields = '__all__'
+
+    def get_lesson_count(self, obj):
+        return Lesson.objects.filter(course=obj).count()
 
 
 class LessonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lesson
+        fields = '__all__'
+
+
+class PaymentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Payment
         fields = '__all__'
