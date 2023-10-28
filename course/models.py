5041450ce476ee5baaf5951eeb11c347fd0b664d
Andrey Ryazanov <ark2635@gmail.com>
2023-10-28 11:11:46 +0300

    create new model 24_2

--- course/models.py
+++ course/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from users.models import User
+
 
 class Course(models.Model):
     title = models.CharField(max_length=20, verbose_name='Название')
@@ -19,3 +21,20 @@ class Lesson(models.Model):
 
     def __str__(self):
         return f'{self.title}'
+
+
+class Payment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateField()
+    course = models.ForeignKey('course.Course', on_delete=models.CASCADE, null=True, blank=True,
+                               verbose_name="оплаченный курс")
+    lesson = models.ForeignKey('course.Lesson', on_delete=models.CASCADE, null=True, blank=True,
+                               verbose_name="оплаченный урок")
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    payment_method = models.CharField(max_length=20,
+                                      choices=[('cash', 'Наличные'), ('transfer', 'Перевод на счет')])
+
+    def __str__(self):
+        return f"{self.user.username} - {self.date}"
+
+
