17f0953ffc3b734ed8ae6e7bf8f48281331b4df6
Andrey Ryazanov <ark2635@gmail.com>
2023-11-01 12:12:48 +0300

    add role

--- users/models.py
+++ users/models.py
@@ -1,5 +1,11 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import gettext_lazy
+
+
+class UserRoles(models.TextChoices):
+    MEMBER = 'member', gettext_lazy('member')
+    MODERATOR = 'moderator', gettext_lazy('moderator')
 
 
 class User(AbstractUser):
@@ -8,6 +14,7 @@ class User(AbstractUser):
     avatar = models.ImageField(upload_to='users/', null=True, blank=True, verbose_name='Аватар')
     phone = models.CharField(max_length=20, verbose_name='Телефон')
     country = models.CharField(max_length=20, verbose_name='Страна')
+    role = models.CharField(max_length=10, choices=UserRoles.choices, default=UserRoles.MEMBER)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
