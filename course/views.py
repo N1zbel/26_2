d4a71b5fe9884419ffa63beda127162b06049027
Andrey Ryazanov <ark2635@gmail.com>
2023-11-01 12:14:54 +0300

    add permissions

--- course/views.py
+++ course/views.py
@@ -5,34 +5,53 @@ from .filters import PaymentFilter
 from .models import Course, Lesson, Payment
 from .serializers import CourseSerializer, LessonSerializer, PaymentSerializer
 from rest_framework.generics import ListAPIView, CreateAPIView, UpdateAPIView, RetrieveAPIView, DestroyAPIView
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from permissions import IsOwner, IsModerator
 
 
 class LessonListAPIView(ListAPIView):
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
+    permission_classes = [IsAuthenticated]
 
 
 class LessonCreateAPIView(CreateAPIView):
     serializer_class = LessonSerializer
+    permission_classes = [IsAdminUser]
 
 
 class LessonDestroyAPIView(DestroyAPIView):
     queryset = Lesson.objects.all()
+    permission_classes = [IsOwner | IsAdminUser]
 
 
 class LessonUpdateAPIView(UpdateAPIView):
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
+    permission_classes = [IsOwner | IsModerator | IsAdminUser]
 
 
 class LessonRetrieveAPIView(RetrieveAPIView):
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
+    permission_classes = [IsOwner | IsModerator | IsAdminUser]
 
 
 class CourseViewSet(viewsets.ModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
+
+    def get_permissions(self):
+        action_permissions = {
+            'retrieve': [IsOwner | IsModerator | IsAdminUser],
+            'create': [IsAdminUser],
+            'destroy': [IsOwner | IsAdminUser],
+            'update': [IsOwner | IsModerator | IsAdminUser],
+        }
+
+        default_permissions = [IsAuthenticated]
+
+        return [permission() for permission in action_permissions.get(self.action, default_permissions)]
 
 
 class PaymentListView(generics.ListAPIView):
@@ -40,3 +59,4 @@ class PaymentListView(generics.ListAPIView):
     serializer_class = PaymentSerializer
     filter_backends = [DjangoFilterBackend]
     filterset_class = PaymentFilter
+    permission_classes = [IsAuthenticated]
