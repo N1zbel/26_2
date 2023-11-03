9de5c1054435fabb50c92e579be404132dab1fc5
Andrey Ryazanov <ark2635@gmail.com>
2023-11-03 11:19:57 +0300

    upd serializers

--- course/serializers.py
+++ course/serializers.py
@@ -1,10 +1,17 @@
 from rest_framework import serializers
 
-from .models import Course, Lesson, Payment
+from .models import Course, Lesson, Payment, Subscription
+
+
+class SubscriptionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Subscription
+        fields = '__all__'
 
 
 class CourseSerializer(serializers.ModelSerializer):
     lesson_count = serializers.SerializerMethodField()
+    is_subscribed = serializers.SerializerMethodField()
 
     class Meta:
         model = Course
@@ -12,6 +19,11 @@ class CourseSerializer(serializers.ModelSerializer):
 
     def get_lesson_count(self, obj):
         return Lesson.objects.filter(course=obj).count()
+
+    def get_is_subscribed(self, obj):
+        user = self.context['request'].user
+
+        return Subscription.objects.filter(user=user, course=obj).exists()
 
 
 class LessonSerializer(serializers.ModelSerializer):
