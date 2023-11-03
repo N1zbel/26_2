34ae799affb0e54b4e9bb2ce9a29f513c431bb94
Andrey Ryazanov <ark2635@gmail.com>
2023-11-03 11:20:33 +0300

    upd views

--- course/views.py
+++ course/views.py
@@ -1,9 +1,11 @@
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import viewsets, generics
+from rest_framework import viewsets, generics, status
+from rest_framework.response import Response
 
 from .filters import PaymentFilter
-from .models import Course, Lesson, Payment
-from .serializers import CourseSerializer, LessonSerializer, PaymentSerializer
+from .models import Course, Lesson, Payment, Subscription
+from .paginators import CoursePaginator, LessonPaginator
+from .serializers import CourseSerializer, LessonSerializer, PaymentSerializer, SubscriptionSerializer
 from rest_framework.generics import ListAPIView, CreateAPIView, UpdateAPIView, RetrieveAPIView, DestroyAPIView
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from course.permissions import IsOwner, IsModerator
@@ -13,6 +15,7 @@ class LessonListAPIView(ListAPIView):
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
     permission_classes = [IsAuthenticated]
+    pagination_class = LessonPaginator
 
 
 class LessonCreateAPIView(CreateAPIView):
@@ -40,6 +43,7 @@ class LessonRetrieveAPIView(RetrieveAPIView):
 class CourseViewSet(viewsets.ModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
+    pagination_class = CoursePaginator
 
     def get_permissions(self):
         action_permissions = {
@@ -60,3 +64,38 @@ class PaymentListView(generics.ListAPIView):
     filter_backends = [DjangoFilterBackend]
     filterset_class = PaymentFilter
     permission_classes = [IsAuthenticated]
+
+
+class SubscribeCourseView(generics.CreateAPIView):
+    queryset = Subscription.objects.all()
+    serializer_class = SubscriptionSerializer
+    permission_classes = [IsAuthenticated]
+
+    def create(self, request, *args, **kwargs):
+        course_id = kwargs.get('course_id')
+        course = Course.objects.get(pk=course_id)
+
+        if Subscription.objects.filter(user=request.user, course=course).exists():
+            return Response({'detail': 'Вы уже подписаны'}, status=status.HTTP_400_BAD_REQUEST)
+
+        serializer = self.get_serializer(data={'user': request.user.id, 'course': course.id})
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+
+        return Response({'detail': 'Вы подписались на курс.'}, status=status.HTTP_201_CREATED)
+
+
+class UnsubscribeCourseView(generics.DestroyAPIView):
+    queryset = Subscription.objects.all()
+    serializer_class = SubscriptionSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        course_id = self.kwargs.get('course_id')
+        course = Course.objects.get(pk=course_id)
+        return Subscription.objects.get(user=self.request.user, course=course)
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response({'detail': 'Вы отписались от курса.'}, status=status.HTTP_200_OK)
