9df3ed64ed2f6a6db284fe0f8129d7c6ce132fb3
Andrey Ryazanov <ark2635@gmail.com>
2023-11-08 12:59:00 +0300

    add new app drf_yasg

--- DRF/settings.py
+++ DRF/settings.py
@@ -30,7 +30,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    'drf_yasg',
     'users',
     'course',
     'rest_framework',
