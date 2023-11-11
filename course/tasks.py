6adf7abe93f2d47e74cdb49cd52509719e9288d8
Andrey Ryazanov <ark2635@gmail.com>
2023-11-11 11:23:28 +0300

    upd

--- course/tasks.py
+++ course/tasks.py
@@ -5,27 +5,28 @@ from django.utils import timezone
 from celery import shared_task
 from django.core.mail import EmailMessage
 
-from .models import User
+from .models import User, Course
 
 
 @shared_task
-def course_update_mail(obj):
-    subject = f'Обновление курса {obj.title}'
-    message = 'Произошло обновление курса'
-    from_email = settings.EMAIL_HOST_USER
-    recipient_list = []
-    subscription_list = obj.subscription_set.all()
-    for subscription in subscription_list:
-        recipient_list.append(subscription.user)
+def course_update_mail(course_id: int) -> None:
+    course = Course.objects.get(pk=course_id)
 
-    if len(subscription_list) > 0:
+    recipient_list = [
+        subscription.user
+        for subscription in course.subscription_set.all()
+    ]
+
+    if recipient_list:
+        subject = f'Обновление курса {obj.title}'
+        message = 'Произошло обновление курса'
+        from_email = settings.EMAIL_HOST_USER
         email = EmailMessage(subject, message, from_email, recipient_list)
         email.send()
 
 @shared_task
 def check_inactive_users():
     one_month_ago = timezone.now() - timedelta(days=30)
-    inactive_users = User.objects.filter(last_login__lte=one_month_ago, is_active=True)
-    for user in inactive_users:
-        user.is_active = False
-        user.save()
+    User.objects.filter(
+        last_login__lte=one_month_ago, is_active=True
+    ).update(is_active=False)
